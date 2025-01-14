e2c9c923a3846d987c32007a13ba750a5760e258
AbdullahBera <kucukabdullahbera@gmail.com>
2025-01-13 20:38:58 -0800

    day 10

--- sql_questions.sql
+++ sql_questions.sql
@@ -443,3 +443,25 @@ SELECT hotel_name,
 		COUNT(reviewer_score) AS number_of_scores
 FROM hotel_reviews
 GROUP BY hotel_name, reviewer_score
+
+
+-- Question 14 (Amazon)
+"""
+𝐌𝐮𝐬𝐭 𝐓𝐫𝐲: Amazon, Salesforce (Basic Level) hashtag#SQL Interview Question — Solution
+
+What is the total sales revenue of Samantha and Lisa?
+
+🔊 Just wanted to point out that even major companies like Amazon ask questions this basic— so it's worth giving it a try! and share the output! 👇
+
+𝐒𝐜𝐡𝐞𝐦𝐚 𝐚𝐧𝐝 𝐃𝐚𝐭𝐚𝐬𝐞𝐭:
+CREATE TABLE sales_performance (salesperson VARCHAR(50),widget_sales INT,sales_revenue INT,id INT PRIMARY KEY);
+
+INSERT INTO sales_performance (salesperson, widget_sales, sales_revenue, id) VALUES('Jim', 810, 40500, 1),('Bobby', 661, 33050, 2),('Samantha', 1006, 50300, 3),
+('Taylor', 984, 49200, 4),('Tom', 403, 20150, 5),('Pat', 715, 35750, 6),('Lisa', 1247, 62350, 7);
+------------
+"""
+
+SELECT
+		SUM(sales_revenue) AS total_revenue 
+FROM sales_performance
+WHERE salesperson IN ('Samantha', 'Lisa')
