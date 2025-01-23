2e996ec9946854051a73f53e17c546aff21372ff
AbdullahBera <kucukabdullahbera@gmail.com>
2025-01-22 20:46:40 -0800

    day 16

--- sql_questions.sql
+++ sql_questions.sql
@@ -598,10 +598,47 @@ INSERT INTO title(worker_ref_id, worker_title, affected_from) VALUES(1, 'Enginee
 -----------
 """
 
-
 SELECT t.worker_title
 FROM worker AS w
 JOIN title AS t ON t.worker_ref_id = w.worker_id
 WHERE w.salary = (
 		SELECT MAX(salary) FROM worker
 );
+
+
+-- Question 19 (Walmart)
+
+"""
+𝐌𝐮𝐬𝐭 𝐓𝐫𝐲: Walmart (HardLevel) hashtag#SQL Interview Question — Solution
+
+Identify users who started a session and placed an order on the same day. For these users, calculate the total number of orders and the total order value for that day. 
+Your output should include the user, the session date, the total number of orders, and the total order value for that day.
+
+🔍By solving this, you'll learn how to use join, groupby and having. Give it a try and share the output! 👇
+
+𝐒𝐜𝐡𝐞𝐦𝐚 𝐚𝐧𝐝 𝐃𝐚𝐭𝐚𝐬𝐞𝐭:
+CREATE TABLE sessions(session_id INT PRIMARY KEY,user_id INT,session_date DATETIME);
+
+INSERT INTO sessions(session_id, user_id, session_date) VALUES (1, 1, '2024-01-01 00:00:00'),(2, 2, '2024-01-02 00:00:00'),(3, 3, '2024-01-05 00:00:00'),(4, 3, 
+'2024-01-05 00:00:00'),(5, 4, '2024-01-03 00:00:00'),(6, 4, '2024-01-03 00:00:00'),(7, 5, '2024-01-04 00:00:00'),(8, 5, '2024-01-04 00:00:00'),(9, 3, '2024-01-05 00:00:00'),
+(10, 5, '2024-01-04 00:00:00');
+
+CREATE TABLE order_summary (order_id INT PRIMARY KEY,user_id INT,order_value INT,order_date DATETIME);
+
+INSERT INTO order_summary (order_id, user_id, order_value, order_date) VALUES (1, 1, 152, '2024-01-01 00:00:00'),(2, 2, 485, '2024-01-02 00:00:00'),(3, 3, 398, 
+'2024-01-05 00:00:00'),(4, 3, 320, '2024-01-05 00:00:00'),(5, 4, 156, '2024-01-03 00:00:00'),(6, 4, 121, '2024-01-03 00:00:00'),(7, 5, 238, '2024-01-04 00:00:00'),
+(8, 5, 70, '2024-01-04 00:00:00'),(9, 3, 152, '2024-01-05 00:00:00'),(10, 5, 171, '2024-01-04 00:00:00');
+
+-----------
+"""
+
+SELECT s.user_id,
+		s.session_date,
+		COUNT(o.order_id) AS count_order,
+		sum(o.order_value) AS total_value
+FROM sessions AS s 
+JOIN order_summary AS o 
+	ON o.user_id = s.user_id
+WHERE s.session_date = o.order_date 
+GROUP BY s.user_id, s.session_date
+ORDER BY 2
