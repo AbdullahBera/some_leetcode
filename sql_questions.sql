b59273a814af00b809225ef11d1b7054c4540a7c
AbdullahBera <kucukabdullahbera@gmail.com>
2025-02-16 12:07:42 -0800

    day 24

--- sql_questions.sql
+++ sql_questions.sql
@@ -913,7 +913,7 @@ WITH transactions AS(
 		ON c.id = r.cust_id 
 	WHERE r.rank=3
 
--- Question 26 
+-- Question 26 (Amazon)
 
 """
 𝐌𝐮𝐬𝐭 𝐓𝐫𝐲: Amazon (Hard Level) hashtag#SQL Interview Question — Solution
@@ -993,3 +993,35 @@ WITH top_amazon AS (
 	LEFT JOIN not_exclusive AS ne
 		ON a.product_name = ne.product_name
 	WHERE ne.product_name IS NULL
+
+-- Question 27 (LinkedIn)
+
+"""
+𝐌𝐮𝐬𝐭 𝐓𝐫𝐲: Linkedln (Hard Level) hashtag#SQL Interview Question — Solution
+
+Consider all LinkedIn users who, at some point, worked at Microsoft. For how many of them was Google their next employer right after Microsoft 
+(no employers in between)?
+
+🌀 Trust me, this one will seriously twist your brain! Give it a try and share the output! 👇
+
+𝐒𝐜𝐡𝐞𝐦𝐚 𝐚𝐧𝐝 𝐃𝐚𝐭𝐚𝐬𝐞𝐭:
+CREATE TABLE linkedin_users (user_id INT,employer VARCHAR(255),position VARCHAR(255),start_date DATETIME,end_date DATETIME);
+
+INSERT INTO linkedin_users (user_id, employer, position, start_date, end_date) VALUES(1, 'Microsoft', 'developer', '2020-04-13', '2021-11-01'),(1, 'Google', 
+'developer', '2021-11-01', NULL),(2, 'Google', 'manager', '2021-01-01', '2021-01-11'),(2, 'Microsoft', 'manager', '2021-01-11', NULL),(3, 'Microsoft', 'analyst', 
+'2019-03-15', '2020-07-24'),(3, 'Amazon', 'analyst', '2020-08-01', '2020-11-01'),(3, 'Google', 'senior analyst', '2020-11-01', '2021-03-04'),(4, 'Google', 'junior developer',
+ '2018-06-01', '2021-11-01'),(4, 'Google', 'senior developer', '2021-11-01', NULL),(5, 'Microsoft', 'manager', '2017-09-26', NULL),(6, 'Google', 'CEO', '2015-10-02', NULL);
+"""
+
+WITH lead_employer AS(
+	SELECT user_id,
+			employer,
+			start_date,
+			end_date,
+			LEAD(employer) OVER (PARTITION BY user_id ORDER BY start_date) AS next_employer 
+	FROM linkedin_users
+)
+	SELECT user_id
+	FROM lead_employer
+	WHERE employer = 'Microsoft' AND next_employer = 'Google'
+	
